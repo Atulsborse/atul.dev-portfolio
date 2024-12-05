bc045dc54a35cfabd96248c75ac5086ac22aeebc
Atulsborse <116156328+Atulsborse@users.noreply.github.com>
2024-12-05 14:10:49 +0530

    Update Contact.tsx
    local urlchange to renderurl

--- project/src/components/Contact.tsx
+++ project/src/components/Contact.tsx
@@ -19,7 +19,7 @@ export default function Contact() {
     };
   
     try {
-      const response = await fetch('http://localhost:5000/send-email', {
+      const response = await fetch('https://atul-dev-portfolio.onrender.com', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data),
